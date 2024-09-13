ef6b3c415ff63c8a7683c7672f4f39ea4bfdb491
yubelgg <yulongwangbusiness@gmail.com>
2024-09-13 18:43:34 -0400

    sign in and out

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,15 @@ import { signIn, signOut } from "next-auth/react";
 export default function Home() {
   const { data: session } = useSession();
   if (session) {
-    <div>Welcome</div>;
+    <div>
+      <p
+        className="opacity-70 mt-8 mb-5 underline cursor-pointer"
+        onClick={() => signOut()}
+      >
+        Sign Out
+      </p>
+      Welcome
+    </div>;
   } else {
     return (
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
