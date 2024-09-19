8b9d34d84b93dbe122b5005ecc1c1fda7aa78ecb
yubelgg <yulongwangbusiness@gmail.com>
2024-09-19 01:36:51 -0400

    simple signin

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,32 +1,15 @@
-import NextAuth from "next-auth/next";
-import { type NextAuthOptions } from "next-auth";
-import SpotifyProvider from "next-auth/providers/spotify";
+import NextAuth from "next-auth";
+import Spotify from "next-auth/providers/spotify";
 
-const options: NextAuthOptions = {
+const handler = NextAuth({
   providers: [
-    SpotifyProvider({
+    Spotify({
       authorization:
         "https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,playlist-modify-private,playlist-modify-public",
       clientId: process.env.SPOTIFY_CLIENT_ID || "",
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET || "",
     }),
   ],
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.access_token = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      return {
-        ...session,
-        token,
-      };
-    },
-  },
-};
-
-const handler = NextAuth(options);
+});
 
 export { handler as GET, handler as POST };
