b0470324111dbfc852f24f5c428f15395541050d
yubelgg <yulongwangbusiness@gmail.com>
2024-09-20 15:17:39 -0400

    moved auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,5 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
-import SpotifyProvider from "next-auth/providers/spotify";
-
-declare module "next-auth" {
-  interface Session {
-    accessToken?: string;
-  }
-}
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    SpotifyProvider({
-      clientId: process.env.SPOTIFY_CLIENT_ID || "",
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET || "",
-      authorization:
-        "https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,playlist-modify-private,playlist-modify-public",
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      session.accessToken = token.accessToken as string;
-      return session;
-    },
-  },
-  debug: true,
-};
+import { authOptions } from "@/app/lib/auth";
 
 const handler = NextAuth(authOptions);
 
--- app/lib/spotify.tsx
+++ app/lib/spotify.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/lib/auth";
 
 export async function getCurrentUser() {
   const session = await getServerSession(authOptions);
