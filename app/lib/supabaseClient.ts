ba0e2a01df91284fd2df73c9c3e11e815dd9362e
yubelgg <yulongwangbusiness@gmail.com>
2024-10-03 15:59:10 -0400

    build: changed env name

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_SUPABASE_ANON_KEY!;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Missing Supabase environment variables')
+}
 
 const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
 
--- app/lib/supabaseServer.ts
+++ app/lib/supabaseServer.ts
@@ -3,6 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
+if (!supabaseUrl || !supabaseServiceRoleKey) {
+    throw new Error('Missing Supabase environment variables')
+}
+
 const supabaseServer = createClient(supabaseUrl, supabaseServiceRoleKey);
 
 export default supabaseServer;
